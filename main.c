d89109217a9e7834457269ef0d6fde3cd4d5ce2f
Olivier El Mekki <olivier@el-mekki.com>
2023-11-19 12:21:01 +0100

    ADD do the parsing

--- main.c
+++ main.c
@@ -3,9 +3,13 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define MAX_LINE_LEN 5000
 bool ONE_LINER = false;
 
-void
+/*
+ * Prints usage.
+ */
+static void
 usage (const char *progname)
 {
   fprintf (stderr, " \
@@ -18,10 +22,59 @@ Options: \n\
 `-1`: print all in one line (eg to facilitate grepping). \n", progname);
 }
 
+/*
+ * Replace unicode entities quotes with ASCII ones.
+ */
+void
+replace_quotes (char line[MAX_LINE_LEN])
+{
+  char modified_line[MAX_LINE_LEN] = {0};
+
+  snprintf (modified_line, MAX_LINE_LEN - 1, "%s", line);
+  char *ptr = line;
+
+  while ((ptr = strstr (ptr, "&#39;")))
+    {
+      snprintf (modified_line + (ptr - line), MAX_LINE_LEN, "'%s", ptr + 5);
+      ptr += 5;
+    }
+
+  int wanna_write = snprintf (line, MAX_LINE_LEN - 1,  "%s", modified_line);
+  if (wanna_write > MAX_LINE_LEN - 1)
+    fprintf (stderr, "replace_quotes(): Warning: line truncated.\n");
+}
+
+/*
+ * Convert given line to plain text.
+ */
+void
+process (const char line[MAX_LINE_LEN])
+{
+  if (strncmp (line, "<p", 2) != 0)
+    return;
+
+  char *ptr = (char *) line;
+  ptr += strcspn (ptr, ">") + 1;
+  size_t len = strcspn (ptr, "<") + 1;
+  char output[MAX_LINE_LEN] = {0};
+  snprintf (output, len, "%s", ptr);
+
+  printf ("%s", output);
+
+  if (ONE_LINER)
+    putchar (' ');
+  else
+    putchar ('\n');
+}
+
 int
 main (int argc, char **argv)
 {
-  if (argc > 1 && strncmp (argv[1], "-1", 3))
+  int err = 0;
+  FILE *file = NULL;
+  char line[MAX_LINE_LEN] = {0};
+
+  if (argc > 1 && strncmp (argv[1], "-1", 3) == 0)
     ONE_LINER = true;
 
   if (argc == 1 || (ONE_LINER && argc == 2))
@@ -30,4 +83,21 @@ main (int argc, char **argv)
       usage (argv[0]);
       exit (1);
     }
+
+  const char *filename = ONE_LINER ? argv[2] : argv[1];
+  file = fopen (filename, "r");
+  if (!file)
+    {
+      perror ("Can't open file");
+      exit (1);
+    }
+
+  while (fgets (line, MAX_LINE_LEN, file))
+    {
+      replace_quotes (line);
+      process (line);
+    }
+
+  if (file) fclose (file);
+  return err;
 }
